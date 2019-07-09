934d43dc3dc018d9d4fc0d5eca16c38fd207c84c
cborjah <cpborja32@gmail.com>
2019-07-08 17:59:55 -0700

    code clean up

--- App.tsx
+++ App.tsx
@@ -8,22 +8,7 @@
 
 import React from 'react';
 import { Fragment } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, ScrollView, View, Text, StatusBar } from 'react-native';
 
 import { Hello } from './src/components/Hello';
 
@@ -37,32 +22,5 @@ const App = () => {
     </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: '700'
-  }
-});
 
 export default App;
