952e916e9b3cbff0f8bd7502fbd6a85523f217a6
cborjah <cpborja32@gmail.com>
2019-07-08 17:49:16 -0700

    sets single quote rule for prettier, changes to prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   root: true,
-  extends: ['@react-native-community', 'prettier'],
+  extends: ["@react-native-community", "plugin:prettier/recommended"],
   rules: {
-    quotes: [2, "single", { "avoidEscape": true }]
+    quotes: [2, "single", { "avoidEscape": true }],
+    "prettier/prettier": [
+      "error",
+      {
+        "singleQuote": true,
+      }
+    ]
   }
 };
